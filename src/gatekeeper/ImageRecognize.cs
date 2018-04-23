663ded27819502b7272e9def3ed6e757a47572b3
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2018-04-23 15:06:39 +0100

    OpenCvDone

--- src/gatekeeper/ImageRecognize.cs
+++ src/gatekeeper/ImageRecognize.cs
@@ -7,13 +7,39 @@ namespace gatekeeper
 {
     public class ImageRecognizer
     {
-        public void DetectIfThereIsFace()
+        CascadeClassifier _profileClassifier;
+        CascadeClassifier _frontalFace;
+        public ImageRecognizer()
         {
-
+            _profileClassifier = new OpenCvSharp.CascadeClassifier();
+            _profileClassifier.Load(".\\models\\haarcascade_profileface.xml");
+            _frontalFace = new OpenCvSharp.CascadeClassifier();
+            _frontalFace.Load(".\\models\\haarcascade_frontalface_default.xml");
+        }
+        public Rect[] DetectFaces(Mat source)
+        {
+            var greyMat = source.CvtColor(ColorConversionCodes.BGR2GRAY);
+            var frontalRectangles = _frontalFace.DetectMultiScale(greyMat);
+            var profileRectangles = _profileClassifier.DetectMultiScale(greyMat);
+            Rect[] result = null;
+            if(frontalRectangles != null && frontalRectangles.Length > 0)
+            foreach (var profile in profileRectangles)
+            {
+                source.Rectangle(profile.Location, profile.BottomRight, Scalar.Blue, 3);
+                Console.Write("Found Profile: ");
+                Console.WriteLine(profile);
+            }
+            foreach (var frontal in frontalRectangles)
+            {
+                source.Rectangle(frontal.Location, frontal.BottomRight, Scalar.Gold, 3);
+                Console.Write("Found Frontal: ");
+                Console.WriteLine(frontal);
+            }
+            return result;
         }
         public void DetectIdentity()
         {
-            
+
         }
     }
 }
--- src/gatekeeper/Program.cs
+++ src/gatekeeper/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using OpenCvSharp;
 
 
@@ -9,17 +10,20 @@ namespace gatekeeper
     {
         static void Main(string[] args)
         {
+
             var vc = new OpenCvSharp.VideoCapture();
             vc.Open(0);
-            var ready = vc.RetrieveMat();
-            Console.WriteLine(ready);
-            vc.Read(ready);
-            vc.Grab();
-            vc.Retrieve(null, 0);
-            Console.WriteLine(ready);
-            Console.WriteLine("Hello World!");
-        }
+            ImageRecognizer imageRecognizer = new ImageRecognizer();
 
-        
+            
+            while (true)
+            {
+                vc.Grab();
+                var mat = vc.RetrieveMat();
+                imageRecognizer.DetectFaces(mat);
+                mat.SaveImage($"./results/{Guid.NewGuid()}.jpg");
+                Thread.Sleep(300);
+            }
+        }
     }
 }
