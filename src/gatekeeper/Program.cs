836718908c1b5e02c0a58d9d7ad60ce41f6c3703
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2018-04-23 11:50:33 +0100

    First project structure

---
+++ src/gatekeeper/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace gatekeeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
